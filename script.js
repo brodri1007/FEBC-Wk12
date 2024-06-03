ba1c3a101c1e81783ff41ad90e7f6bc2c77e8367
Byron Rodriguez <byronrod@gmail.com>
2024-06-03 13:17:04 -0400

    final files completed for real

--- script.js
+++ script.js
@@ -2,22 +2,20 @@
 const url = 'http://localhost:3000/cars'
 
 
-
-
 $.get(url).then((data) =>
     data.map((car) => {
         $('#car_list').append(
-            $(`             
-                     <div class="col pb-5 pt-5 text-light bg-danger text-center">
+            $(`           
+                     <div class="col p-3 border bg-light">
                         <div><span>${car.id}</span> <img alt="${car.model}" src="car.png" width="150px"/></div>
-                        
                         <span>${car.make}</span> 
                         <span>${car.model}</span> <br> 
-                        <span>Miles: ${car.mileage}</span> <br> 
+                        <span>Miles: ${car.mileage}</span>  
                         <span>Year: ${car.year}</span><br>  
-                        <span>Price: $${car.price}</span> <br> 
+                        <span>Price: $${car.price}</span>  
                         <button onclick="deleteCar(${car.id})"}>🗑</button>
-                    </div>              
+                    </div>      
+                            
             `)
         )
     })
@@ -26,11 +24,10 @@ $.get(url).then((data) =>
 
 
 
-
 function deleteCar(id) {
 
     fetch(`${url}/${id}`, {
-      method: "DELETE", 
+        method: "DELETE",
     })
 
 }
@@ -38,43 +35,102 @@ function deleteCar(id) {
 
 function addCar() {
 
-    fetch(url, {
-        method: "POST",
-        body: JSON.stringify({
-            id: $('#idUpdate').val(),
-            make: $('#makeUpdate').val(),
-            model: $('#modelUpdate').val(),
-            mileage: $('#mileageUpdate').val(),
-            year: $('#yearUpdate').val(),
-            price: $('#priceUpdate').val()
-        }),
-        headers: { "Content-type": "application/json; charset=UTF-8" }
-    });
-    
+    // fetch(url, {
+    //     method: "POST",
+    //     body: JSON.stringify({
+    //         id: $('#idUpdate').val(),
+    //         make: $('#makeUpdate').val(),
+    //         model: $('#modelUpdate').val(),
+    //         mileage: $('#mileageUpdate').val(),
+    //         year: $('#yearUpdate').val(),
+    //         price: $('#priceUpdate').val()
+    //     }),
+    //     headers: { "Content-type": "application/json; charset=UTF-8" }
+    // });
+
+
+
+    if ($('#idUpdate').val() != "") {
+
+        $.ajax({
+            type: 'POST',
+            url: url,
+            contentType: 'application/json',
+            data: JSON.stringify({
+                "id": $('#idUpdate').val(),
+                "make": $('#makeUpdate').val(),
+                "model": $('#modelUpdate').val(),
+                "mileage": $('#mileageUpdate').val(),
+                "year": $('#yearUpdate').val(),
+                "price": $('#priceUpdate').val()
+            }),
+            headers: { "Content-type": "application/json; charset=UTF-8" } // access in body
+        }).done(function () {
+            console.log('SUCCESS');
+        }).fail(function (msg) {
+            console.log('FAIL');
+        }).always(function (msg) {
+            console.log('ALWAYS');
+        });
+    } else {
+        alert("Please enter some information.")
+    }
+
 }
 
 
 
 
-function updateCar() { 
-  
-    const requestOptions = {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            id: $('#idUpdate').val(),
-            make: $('#makeUpdate').val(),
-            model: $('#modelUpdate').val(),
-            mileage: $('#mileageUpdate').val(),
-            year: $('#yearUpdate').val(),
-            price: $('#priceUpdate').val()
-        })
-    };
-  
-    //alert(requestOptions.body);
-  
-    fetch(url + '/' + $('#idUpdate').val(), requestOptions)
-        .then(response => response.json());
-        
-  };
+function updateCar() {
+
+    // const requestOptions = {
+    //     method: 'PUT',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({
+    //         id: $('#idUpdate').val(),
+    //         make: $('#makeUpdate').val(),
+    //         model: $('#modelUpdate').val(),
+    //         mileage: $('#mileageUpdate').val(),
+    //         year: $('#yearUpdate').val(),
+    //         price: $('#priceUpdate').val()
+    //     })
+    // };
+
+    // //alert(requestOptions.body);
+
+    // fetch(url + '/' + $('#idUpdate').val(), requestOptions)
+    //     .then(response => response.json());
+    // JSON.stringify({
+    //     "firstname": fname,
+    //     "lastname": lname,
+    //     "grade": grade    
+    //   })
+
+
+    if ($('#idUpdate').val() != "") {
+        $.ajax({
+            type: 'PUT',
+            url: url + '/' + $('#idUpdate').val(),
+            contentType: 'application/json',
+            data: JSON.stringify({
+                "id": $('#idUpdate').val(),
+                "make": $('#makeUpdate').val(),
+                "model": $('#modelUpdate').val(),
+                "mileage": $('#mileageUpdate').val(),
+                "year": $('#yearUpdate').val(),
+                "price": $('#priceUpdate').val()
+            }), // access in body
+        }).done(function () {
+            console.log('SUCCESS');
+        }).fail(function (msg) {
+            console.log('FAIL');
+        }).always(function (msg) {
+            console.log('ALWAYS');
+        });
+    }
+    else {
+        alert("Please enter some information.")
+    }
+
+};
 
